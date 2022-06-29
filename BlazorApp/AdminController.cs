83f96ab49a20230b57519e47f592919937645780
Seenu Pandiyan <seenu.sf3386@gmail.com>
2022-06-29 19:35:54 +0530

    Sample added

---
+++ BlazorApp/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace BlazorApp
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Login([FromForm] string account)
+        {
+            if (account == "user")
+            {
+                List<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Role, "user")
+                };
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "auth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                await HttpContext.SignInAsync(claimsPrincipal);
+
+            }
+            if (account == "admin")
+            {
+                List<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Role, "admin")
+                };
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "auth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                await HttpContext.SignInAsync(claimsPrincipal);
+
+            }
+            return Redirect("/adminpage");
+        }
+    }
+}
